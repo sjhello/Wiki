9361359f7abd1578bcb1fe877870f6097ee06a29
sjhello <sjhello235@gmail.com>
2023-06-15 23:03:20 +0900

    ci

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://sjhello.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sjhello', // Usually your GitHub org/user name.
-  projectName: 'sjhello.github.io', // Usually your repo name.
+  projectName: 'wiki', // Usually your repo name.
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
