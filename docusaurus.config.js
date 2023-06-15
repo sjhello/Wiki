c51ed3bd8a0467414d390f23d5b098b0645bb839
sjhello <sjhello235@gmail.com>
2023-06-15 23:22:06 +0900

    ci test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/sjhello',
             label: 'GitHub',
             position: 'right',
           },
